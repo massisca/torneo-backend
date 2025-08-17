f3738ffdc122f201aa438006fdc43ac1600f1701
Massimiliano Scalia <massimiliano.sca@gmail.com>
2025-08-17 20:24:41 +0200

    prova

--- api/login.js
+++ api/login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 // const bcrypt = require('bcrypt');  
-
+ 
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
