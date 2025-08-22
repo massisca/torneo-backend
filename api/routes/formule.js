c156eff95ea0efb8715532466c3f9c45806d5059
Massimiliano Scalia <massimiliano.sca@gmail.com>
2025-08-22 11:07:59 +0200

    sistemato routes

--- api/routes/formule.js
+++ api/routes/formule.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../../db');
 
 // GET tutte le formule
 router.get('/', async (req, res) => {
--- api/routes/ruoli.js
+++ api/routes/ruoli.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../../db');
 
 // GET tutti i ruoli
 router.get('/', async (req, res) => {
